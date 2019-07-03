9501f0c10e99859977095ce778bd678b682a88c9
Shawanda Davis <shawandadavis93@gmail.com>
2019-07-03 13:51:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function scuberGreetingForFeet(x){
   if (x >= 400) {'This one is on me!';}
   else if (x > 2000) {'I will gladly take your thirty bucks.';}
-  else
+  else (x > 2500) {'No can do.';}
     
 }
 
